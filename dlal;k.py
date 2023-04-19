f98a1e1aeab22cebb66f9338fa27cf312fe55cae
Mssoo04 <129568167+Mssoo04@users.noreply.github.com>
2023-04-19 23:55:22 +0900

    Add files via upload

--- dlal;k.py
+++ dlal;k.py
@@ -1,9 +1,13 @@
-#n을 입력 받은 후 1~n까지의 홀수를 출력
-#합을 구하여함
-i = 0
-num = int(input('양의 정수를 입력하시오.'))
-for a in range(1, num+1):
-    if a % 2 ==1:
-        print(num, '까지의 홀수는', a)
-        i += a
-print(num,"까지의 홀수들의 합은 :", i)
+for i in range(1, 100,1):
+    if i < 10:
+        print(i, end='')
+        if i % 3 == 0:
+            print('짝', end='')
+    else:
+        print(i, end='')
+        if (i // 10) % 3 == 0:
+            print('짝',end='')
+        if ((i % 10) % 3 == 0 and (i % 10) != 0)== 0:
+            print('짝', end='')
+
+    print()
